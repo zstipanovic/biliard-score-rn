1aeeaa0c49bd0526ee8259d82ee9433c09d4120f
zstipanovic <zvonimir.stipanovic1@gmail.com>
2024-02-14 12:44:35 +0100

    project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['simple-import-sort', 'import'],
+  rules: {
+    curly: ['error', 'multi-line'],
+    'react-native/no-inline-styles': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
 };
