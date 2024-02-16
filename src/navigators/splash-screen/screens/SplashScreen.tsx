9ded77b058181f7b96fb5db6185a356c88725b9c
zstipanovic <zvonimir.stipanovic1@gmail.com>
2024-02-16 15:47:29 +0100

    added open sans fonts

--- src/navigators/splash-screen/screens/SplashScreen.tsx
+++ src/navigators/splash-screen/screens/SplashScreen.tsx
@@ -1,11 +1,23 @@
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export function SplashScreen(): JSX.Element {
   return (
     <SafeAreaView>
       <View>
-        <Text>Splash Screen</Text>
+        <Text style={styles.font}>Splash Screen</Text>
+        <Text style={styles.fontDva}>Basic font</Text>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  font: {
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 24,
+  },
+
+  fontDva: {
+    fontSize: 24,
+  },
+});
